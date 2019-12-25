d494b4d74f58adb864c9bdb3c8de9d55ad4b22f6
jesusMava <jesusmacedo7@gmail.com>
2019-12-25 15:32:16 -0600

    refactor: New code

--- exercise4.rb
+++ exercise4.rb
@@ -1,58 +1,47 @@
-class Environment
+class Environments
   def variables
-    array_numbers = []
-    until array_numbers.length >= 2
-      puts 'Enter the number '
-      number_n = gets.chomp
-      array_numbers.push(number_n)
+    @array_number = []
+
+    2.times do
+      push_numbers
     end
+
+    loop do
       puts 'Do you want to add another number? y/n'
       answer = gets.chomp
-      while answer == 'y'
-      puts 'Enter the number '
-      number_n = gets.chomp
-      array_numbers.push(number_n)
-      puts 'Do you want to add another number? y/n'
-      answer = gets.chomp
+
+      break if answer === 'n'
+
+      push_numbers
+    end
+
+    @array_number
+  end
+
+  def push_numbers
+    puts 'Enter the number'
+    @array_number.push(gets.to_i)
+  end
+
+  def operator
+    puts 'Please choose an operator'
+    gets.chomp
+  end
+end
+
+class Operations
+  def result(array_numbers, operator)
+
+    first_number = array_numbers.delete_at(0)
+
+      result = array_numbers.reduce(first_number) do |total, number|
+        total.send(operator, number)
       end
-    return array_numbers
+
+    puts "The Result is #{result}"
   end
+
 end
 
-class SingsOperations
-  def operators
-    puts 'please choose an operator'
-    puts '1.(*)  2.(/)  3.(+)  4.(-)'
-    operator = gets.chomp
-    case operator
-    when "1"
-      operator = "*"
-    when "2"
-      operator = "/"
-    when "3"
-      operator = "+"
-    when "4"
-      operator = "-"
-    else
-      puts "Error unknown operator"
-    end
-    return operator
-  end
-end
-
-class Operations 
-  def result_operation(array_numbers, operation_sing)
-    sum = array_numbers.delete_at(0).to_i
-    array_numbers.each{ |num| sum = sum.send(operation_sing, num.to_i) }
-    puts "The result is #{sum}"
-  end
-end
-
-environment = Environment.new
-array_numbers = environment.variables
-
-singsOfOperations = SingsOperations.new
-operation_sing = singsOfOperations.operators
-
-operation = Operations.new
-operation.result_operation(array_numbers, operation_sing)
+environments = Environments.new
+Operations.new.result(environments.variables, environments.operator)
